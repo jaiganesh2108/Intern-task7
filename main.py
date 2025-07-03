97784c2632e55c4aa1b5763316832a1ce17048c3
jaiganesh2108 <jaig7335@gmail.com>
2025-07-03 16:05:28 +0530

    secton asset commit

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from PIL import Image
 import os
 
-def resize(img,new_width):
-    width,height = img.size
+def resize(im, new_width):
+    width, height = im.size
     ratio = height/width
     new_height = int(ratio*new_width)
-    resized_image = img.resize((new_width,new_height))
+    resized_image = im.resize((new_width,new_height))
     return resized_image
 
-files = os.listdir("images")
-extensions = ['jpg','jpeg','png','gif']
+files = os.listdir("assets")
+extensions = ['jpg','png','jpeg']
 for file in files:
-    ext = file.split(".")[-1]
+    ext = file.split('.')[-1]
     if ext in extensions:
-        img = Image.open("images/" + file)
-        img_resized = resize(img,300)
-        file_path = f"images/{file}.jpeg"
-        img_resized.save(file_path)
+        im = Image.open('assets/'+file)
+        im_resized = resize(im, 600)
+        new_dir = 'resized_images'
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+        file = f'{new_dir}/{file}.jpg'
+        im_resized.save(file, 'png', quality=100)
